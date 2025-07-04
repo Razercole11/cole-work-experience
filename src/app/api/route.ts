f8b03c2c93585e65e5daf072819d95402bb95c2d
razercole11 <razercole11@gmail.com>
2025-07-04 11:46:27 +0100

    Done validation maybe?

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import { formDataSchema} from "@/schema";
-import * as z from "zod/v4";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  console.log(body);
-  return new NextResponse("test", {
+  try{
+      const body = await req.json();
+      const result = formDataSchema.safeParse(body);
+      if(!result.success) {
+        const errors = result.error.issues;
+        return NextResponse.json({ error: {message: "Invalid request", errors}}, {status: 400});
+      }
+      console.log(body);
+    return new NextResponse("test", {
       status: 200,
       headers: { "Content-Type": "text/plain" },
     }
-  );
-}
+    );
+  }catch(error) {
+     console.error("Error proccesing POST request:", error);
+     return NextResponse.json({error: {message: "Internal Server Error"}}, {status: 500});
+  }
+} 
